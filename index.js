d5e06300a9f4579d0ab79b59cdc8968c6ecc1b6b
husanjonmusayev <husanjonm279@gmail.com>
2023-12-11 18:18:39 +0500

    uy ishi

---
+++ index.js
@@ -0,0 +1,31 @@
+
+// foretch
+
+// function printArr(arr) {
+//     let sum = 0;
+//    arr.forEach(element => {
+//     if (element > 0) {
+// sum = sum + element;
+// }}); 
+
+//    return sum;
+// }
+
+// console.log(printArr(arr));
+// filter
+// let arr1 = arr.filter((valeu) => {
+//     return valeu % 2 ==0
+// });
+
+// console.log(arr1);
+
+let arr = [1, 2, 3, 4, 5, 6,15]
+// let arr1 = arr.filter((valeu) => {
+//     return valeu%3==0 && valeu%5==0;
+// })
+// console.log(arr1);
+
+let arr1 = arr.map((valeu) => {
+    
+})
+
